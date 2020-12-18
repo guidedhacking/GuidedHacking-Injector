aef733bc4c85821f5077c02754d2a8358e907bf2
Broihon <konradherrmann@t-online.de>
2020-12-18 12:06:08 +0100

    updated version ref to 4.0

--- Injection.h
+++ Injection.h
@@ -13,8 +13,8 @@
 
 #pragma once
 
-#define GH_INJ_VERSIONW L"3.4"
-#define GH_INJ_VERSIONA "3.4"
+#define GH_INJ_VERSIONW L"4.0"
+#define GH_INJ_VERSIONA "4.0"
 
 #define GH_INJ_MOD_NAME64W L"GH Injector - x64.dll"
 #define GH_INJ_MOD_NAME86W L"GH Injector - x86.dll"
